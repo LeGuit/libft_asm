03355c0c5b3f2c2dabd838ced57f3f0de971f1af
Guillaume WOODWARD <gwoodwar@e3r12p11.42.fr>
2016-08-17 18:06:57 +0200

    ft_strlen

--- srcs/ft_strlen.s
+++ srcs/ft_strlen.s
@@ -6,7 +6,7 @@
 ;;   By: gwoodwar <gwoodwar@student.42.fr>          +#+  +:+       +#+        ;;
 ;;                                                +#+#+#+#+#+   +#+           ;;
 ;;   Created: 2016/08/17 14:39:34 by gwoodwar          #+#    #+#             ;;
-;;   Updated: 2016/08/17 17:24:33 by gwoodwar         ###   ########.fr       ;;
+;;   Updated: 2016/08/17 18:06:40 by gwoodwar         ###   ########.fr       ;;
 ;;                                                                            ;;
 ;; ************************************************************************** ;;
 
@@ -14,4 +14,12 @@ global 		ft_strlen
 
 ft_strlen:
 	push	edi
-	
+	sub		ecx, ecx
+	not		ecx
+	mov		edi,[esp+8]
+	sub		al, al			;put '\0' value for end of count
+	cld						;to read from lowest to highest adress
+	repne	scasb			;count till al
+	not		ecx
+	lea		eax, [ecx-1]	;sub the count of the '\0'
+	ret
