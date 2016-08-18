b76f461a0dc909bb43f38b2867fcd04ab4769aa8
Guillaume WOODWARD <gwoodwar@e3r12p9.42.fr>
2016-08-18 17:53:55 +0200

    save

--- srcs/ft_memset.s
+++ srcs/ft_memset.s
@@ -1,39 +1,24 @@
 ;; ************************************************************************** ;;
 ;;                                                                            ;;
 ;;                                                        :::      ::::::::   ;;
-;;   ft_puts.s                                          :+:      :+:    :+:   ;;
+;;   ft_memset.s                                        :+:      :+:    :+:   ;;
 ;;                                                    +:+ +:+         +:+     ;;
 ;;   By: gwoodwar <gwoodwar@student.42.fr>          +#+  +:+       +#+        ;;
 ;;                                                +#+#+#+#+#+   +#+           ;;
-;;   Created: 2016/08/17 14:34:07 by gwoodwar          #+#    #+#             ;;
-;;   Updated: 2016/08/18 17:05:09 by gwoodwar         ###   ########.fr       ;;
+;;   Created: 2016/08/18 17:42:29 by gwoodwar          #+#    #+#             ;;
+;;   Updated: 2016/08/18 17:53:41 by gwoodwar         ###   ########.fr       ;;
 ;;                                                                            ;;
 ;; ************************************************************************** ;;
 
-extern		ft_strlen
+;void	*ft_memset(void *b, int c, size_t len);
+; use rep     stosb maybe
 
-section .data
-	nl			db	10
-	
+ft_memset:
+	push	rdi
+	mov		al, rsi
+	cmp		rdx, 0
+	je		.ret
 
-ft_puts:
-	push	rdi			;to save from strlen
-	call	ft_strlen
-	pop		rdi			;restore after strlen
-	cmp		rax, 0
-	je		.print_nl
-
-.print_string:
-	mov		rsi, rdi	;move str to arg 2
-	mov		rdx, rax	;strlen to arg 3
-	mov		rdi, 1		;fd 1 to arg 1
-	mov		rax, 1		;syscall for write
-	syscall
-
-.print_nl:
-	mov		rsi, nl
-	mov		rdx, 1
-	mov		rdi, 1
-	mov		rax, 1
-	syscall
+.ret
+	pop		rax
 	ret
