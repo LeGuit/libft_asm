e3634100454abe8fbc69e7a8c759b108018f4a65
Guillaume WOODWARD <gwoodwar@e3r12p9.42.fr>
2016-08-18 17:55:47 +0200

    save

--- srcs/ft_memset.s
+++ srcs/ft_memset.s
@@ -6,7 +6,7 @@
 ;;   By: gwoodwar <gwoodwar@student.42.fr>          +#+  +:+       +#+        ;;
 ;;                                                +#+#+#+#+#+   +#+           ;;
 ;;   Created: 2016/08/18 17:42:29 by gwoodwar          #+#    #+#             ;;
-;;   Updated: 2016/08/18 17:53:41 by gwoodwar         ###   ########.fr       ;;
+;;   Updated: 2016/08/18 17:55:39 by gwoodwar         ###   ########.fr       ;;
 ;;                                                                            ;;
 ;; ************************************************************************** ;;
 
@@ -15,8 +15,8 @@
 
 ft_memset:
 	push	rdi
-	mov		al, rsi
-	cmp		rdx, 0
+	mov		al, [rsp + 8] 	;get c
+	mov		rcx, [rsp + 24]	;get len
 	je		.ret
 
 .ret
